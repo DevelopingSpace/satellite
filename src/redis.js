2b16ee1751963699ccfce13a11b4eddca8c1dbf1
Metropass <moho472@gmail.com>
2021-03-19 20:41:06 -0400

    Fixed Prettier Check

--- src/redis.js
+++ src/redis.js
@@ -1,9 +1,9 @@
-const logger = require("./logger");
-const Redis = require("ioredis");
-const MockRedis = require("ioredis-mock");
+const logger = require('./logger');
+const Redis = require('ioredis');
+const MockRedis = require('ioredis-mock');
 
 // If you need to set the Redis URL, do it in REDIS_URL
-const redisUrl = "http://redis:6379";
+const redisUrl = 'http://redis:6379';
 
 // Set MOCK_REDIS=1 to mock, MOCK_REDIS= to use real redis
 const useMockRedis = process.env.MOCK_REDIS;
@@ -26,9 +26,9 @@ function createRedisClient() {
 }
 
 // If using MockRedis, shim info() until https://github.com/stipsan/ioredis-mock/issues/841 ships
-if (useMockRedis && typeof MockRedis.prototype.info !== "function") {
-  logger.debug("Shimming MockRedis info() method");
-  MockRedis.prototype.info = () => Promise.resolve("redis_version:999.999.999");
+if (useMockRedis && typeof MockRedis.prototype.info !== 'function') {
+  logger.debug('Shimming MockRedis info() method');
+  MockRedis.prototype.info = () => Promise.resolve('redis_version:999.999.999');
 }
 
 module.exports = {
